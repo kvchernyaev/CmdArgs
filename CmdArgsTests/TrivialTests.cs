8d5f1f917eeb03c7cb1b94ea16e2252a6277853f
cherniaevkv <cherniaevkv@suek.ru>
2018-03-01 19:45:59 +0300

    #9 tests

---
+++ CmdArgsTests/TrivialTests.cs
@@ -0,0 +1,66 @@
+﻿#region usings
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CmdArgs;
+using NUnit.Framework;
+#endregion
+
+
+
+namespace CmdArgsTests
+{
+    [TestFixture]
+    public class TrivialTests
+    {
+        ////////////////////////////////////////////////////////////////
+        class Conf { }
+
+
+
+        [Test]
+        public void Test()
+        {
+            var p = new CmdArgsParser();
+            Res<Conf> rv = p.ParseCommandLine<Conf>(new string[] { });
+        }
+
+
+        [Test]
+        public void TestM()
+        {
+            var p = new CmdArgsParser();
+            Res<Conf> rv = p.ParseCommandLine<Conf>(new string[] {"-"});
+            Assert.IsTrue(new[] {"-"}.SequenceEqual(rv.AdditionalArguments));
+        }
+
+
+        [Test]
+        public void TestM1()
+        {
+            var p = new CmdArgsParser();
+            Res<Conf> rv = p.ParseCommandLine<Conf>(new string[] {"-1"});
+            Assert.IsTrue(new[] {"-1"}.SequenceEqual(rv.AdditionalArguments));
+        }
+
+
+        [Test]
+        public void TestMM()
+        {
+            var p = new CmdArgsParser();
+            Res<Conf> rv = p.ParseCommandLine<Conf>(new string[] {"--"});
+            Assert.IsTrue(new[] {"--"}.SequenceEqual(rv.AdditionalArguments));
+        }
+
+
+        [Test]
+        public void TestMM1()
+        {
+            var p = new CmdArgsParser();
+            Res<Conf> rv = p.ParseCommandLine<Conf>(new string[] {"--1"});
+            Assert.IsTrue(new[] {"--1"}.SequenceEqual(rv.AdditionalArguments));
+        }
+    }
+}
